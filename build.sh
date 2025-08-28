0bdacbd47285d9abd6110ae285f6788855b64ea2
JLibeau <jeremie.libeau@egerie.eu>
2025-08-28 17:58:14 +0200

    Small update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --no-cache -t loconox/latexcv . && docker run -v `pwd`:/data loconox/latexcv lualatex /data/cv_jeremie_libeau.tex
+docker build -t loconox/latexcv . && docker run -v `pwd`:/data loconox/latexcv lualatex /data/cv_jeremie_libeau.tex
